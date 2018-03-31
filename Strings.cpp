0a85d26220f63c6b02403a64224a13f7ddcb8536
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-31 11:30:38 -0500

    practice

--- Strings.cpp
+++ Strings.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 int main(){
-	char my_string[10] = "Hello You";
-	
+	char my_string[] = "Hello You";
+	int index = 0;
 	cout << my_string << endl;
 	my_string[0]=' ';
+	cout << my_string;
+	while (my_string[index] != '\0'){
+		if (my_string[index] != ' '){
+			my_string[index] = 'X';
+		}
+		index++;
+	}
 	cout << my_string;
 	
 	return 0;
