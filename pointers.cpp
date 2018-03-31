c1ca647aa05e4950ce7260585b7e7bfadc7021fb
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-31 15:18:58 -0500

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int *p1, *p2, *p3, v1, v2;
+	
+	v1 = 0;
+	p1 = &v1;
+	*p1 = 8;
+	p2 = p1;
+	p3 = new int; //nameless variable
+
+
+	cout << p1 << endl << p2 << endl;
+	cout << "Enter a number: ";
+	cin >> *p3;
+	cout << *p3 << " added to 3 is " << *p3+3;
+	cout << endl << "it is located at " << p3;
+	
+	return 0;
+}
