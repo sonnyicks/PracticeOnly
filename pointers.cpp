38533974f2c575ea560a656950b3b76a78cef4de
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-31 15:33:15 -0500

    practice

--- pointers.cpp
+++ pointers.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 
 int main(){
-	int *p1, *p2, *p3, v1, v2;
+	// int *p1, *p2, *p3, v1, v2;
 	
-	v1 = 0;
-	p1 = &v1;
-	*p1 = 8;
-	p2 = p1;
-	p3 = new int; //nameless variable
+	// v1 = 0;
+	// p1 = &v1;
+	// *p1 = 8;
+	// p2 = p1;
+	// p3 = new int; //nameless variable
 
 
-	cout << p1 << endl << p2 << endl;
-	cout << "Enter a number: ";
-	cin >> *p3;
-	cout << *p3 << " added to 3 is " << *p3+3;
-	cout << endl << "it is located at " << p3;
+	// cout << p1 << endl << p2 << endl;
+	// cout << "Enter a number: ";
+	// cin >> *p3;
+	// cout << *p3 << " added to 3 is " << *p3+3;
+	// cout << endl << "it is located at " << p3;
 	
+	int *p1;
+	
+	for(int i=1; i<100; i++){
+		p1 = new int;
+		*p1 = i;
+		cout << *p1;
+	}
 	return 0;
 }
