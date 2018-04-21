5cb9e1d3deca43cafec995982fa0cfa95ec4225d
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-04-21 14:38:07 -0500

    practice

--- Classes2.cpp
+++ Classes2.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 class Computer{
-	private:
+	public:
 	string ios;
 	int ram;
 	int storage;
 	int monitor_size;
-	public:
 	string get_ios(){
 		cout << "Enter IOS: ";
 		cin >> ios;
@@ -30,6 +29,12 @@ class Computer{
 		cin >> monitor_size;
 		return monitor_size;
 	}
+	void print(){
+		cout << "IOS: " << ios << endl
+			 << "RAM: " << ram << endl
+			 << "Storage Space: " << storage << endl
+			 << "Monitor Size: " << monitor_size << endl;
+	}
 	Computer(){
 		get_ios();
 		get_ram();
@@ -37,8 +42,27 @@ class Computer{
 		get_monitor_size();
 	}
 };
+class Intel: public Computer{
+	public:
+	string color;
+	string model;
+	int serial_num;
+	void print(){
+		cout << "IOS: " << ios << endl
+		<< "RAM: " << ram << endl
+		<< "Storage Space: " << storage << endl
+		<< "Monitor Size: " << monitor_size << endl
+		<< "Computer color: " << color << endl
+		<< "Model: " << model << endl
+		<< "Serial Number: " << serial_num << endl;
+	}
+};
 
 int main(){
-	Computer mine;
+	Intel mine;
+	mine.color = "Green";
+	mine.model = "Gamer";
+	mine.serial_num = 554;
+	mine.print();
 	return 0;
 }
